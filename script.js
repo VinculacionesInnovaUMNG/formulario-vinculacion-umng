dcb8bef312d73890a40075f25311c6359790d98f
VinculacionesInnovaUMNG <vinculaciones.innova@unimilitar.edu.co>
2025-06-17 21:51:06 -0500

    Version 1 Formulario
    Se sube la primera versión del formulario para la vinculación OPS

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Formulario enviado correctamente (aún no conectado a una base de datos o Excel).');
+});
